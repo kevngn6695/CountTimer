e147f76367a8c60765e590b465cce6366b36920f
HiepHop <kevngn6695@gmail.com>
2024-04-27 13:43:22 -0700

    Commit more codes

--- client/src/pages/CountdownTimer.js
+++ client/src/pages/CountdownTimer.js
@@ -5,6 +5,19 @@ import Container from "../components/Container";
 
 import "../assets/style/sass/components/timer.sass";
 
+/**
+ * Represents a text input
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.classNameForm - The class name of the form element.
+ * @param {string} props.classNameWrapper - The class name of the wrapper element
+ * @param {string} props.classNameLabel - The class name of the label for the text input element.
+ * @param {string} props.classNameInput - The class name of the input element
+ * @param {number} props.listTextInput - The list text input.
+ * @returns {React.ReactElement} A spiral progressive bar element.
+ */
+
 function CountdownTimer(props) {
   const [hour, setHour] = useState(0);
   const [minute, setMinute] = useState(0);
@@ -30,9 +43,8 @@ function CountdownTimer(props) {
   };
 
   const handleClickStart = () => {
-    if (hour !== 0 || minute !== 0 || second !== 0 || milisecond !== 0) {
+    if (hour !== 0 || minute !== 0 || second !== 0 || milisecond !== 0)
       setIsRunning(true);
-    }
   };
 
   const handleClickReset = () => {
@@ -48,29 +60,16 @@ function CountdownTimer(props) {
   };
 
   const handleTimerLogic = (hour, minute, second, milisecond) => {
-    if (milisecond > 0) {
-      if (milisecond > 100) {
-        setMilisecond(99);
-      }
-      setMilisecond((milisecond) => milisecond - 1);
-    } else if (second > 0) {
-      if (second > 60) {
-        setSecond(59);
-      }
-      setSecond((second) => second - 1);
+    if (milisecond > 0) setMilisecond(milisecond > 100 ? 99 : milisecond - 1);
+    else if (second > 0) {
+      setSecond(second > 60 ? 59 : second - 1);
       setMilisecond(99);
     } else if (minute > 0) {
-      if (minute > 60) {
-        setMinute(59);
-      }
-      setMinute((minute) => minute - 1);
+      setMinute(minute > 60 ? 59 : minute - 1);
       setSecond(59);
       setMilisecond(99);
     } else if (hour > 0) {
-      if (hour > 24) {
-        setHour(24);
-      }
-      setHour((hour) => hour - 1);
+      setHour(hour > 24 ? 24 : hour - 1);
       setMinute(59);
       setSecond(59);
       setMilisecond(99);
